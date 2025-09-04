554930bbea04a6b8cc64dc64506d6157fe8122db
a.hachcham <a.hachcham@egioteam.com>
2025-09-04 11:27:53 +0100

    add tracked data

---
+++ modules/ps_eventbus/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashopcorp/ps_eventbus',
+        'pretty_version' => 'v4.0.11',
+        'version' => '4.0.11.0',
+        'reference' => 'a532620fa88a698c57934056552bccccfab017dd',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashopcorp/ps_eventbus' => array(
+            'pretty_version' => 'v4.0.11',
+            'version' => '4.0.11.0',
+            'reference' => 'a532620fa88a698c57934056552bccccfab017dd',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sentry/sentry' => array(
+            'pretty_version' => '1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => '159eeaa02bb2ef8a8ec669f3c88e4bff7e6a7ffe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sentry/sentry',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
